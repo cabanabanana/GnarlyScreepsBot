e09405d6ff90b959582b02b2f72eb368fd941316
Richard <richard@clicktech.com>
2020-10-10 17:19:16 +0100

    increase harvester limit and spawn upgrader

--- src/roles/spawner.ts
+++ src/roles/spawner.ts
@@ -1,19 +1,31 @@
 namespace Spawner {
     export const run = (creeps : { [creepName: string]: Creep; }) => {
         let harvesters = 0;
+        let upgrader = 0;
 
         Object.keys(creeps).map(creepName => {
             const creep = creeps[creepName];
             if (creep.memory.role === 'harvester') harvesters += 1;
+            if (creep.memory.role === 'upgrader') harvesters += 1;
         });
 
-        if (harvesters < 1) {
+        if (harvesters < 2) {
             const newName = 'Harvester' + Game.time;
 
             Game.spawns['Spawn1'].spawnCreep(
                 [WORK,CARRY,MOVE],
                 newName, 
                 { memory: { role: 'harvester' } }
+            )
+        }
+
+        if (upgrader < 1) {
+            const newName = 'Upgrader' + Game.time;
+
+            Game.spawns['Spawn1'].spawnCreep(
+                [WORK,CARRY,MOVE],
+                newName, 
+                { memory: { role: 'upgrader' } }
             )
         }
     };
