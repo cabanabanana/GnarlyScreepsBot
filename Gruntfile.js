15dd648effe3126442b71e3d1edebe9df04a160e
Richard <richard@clicktech.com>
2020-10-10 15:23:44 +0100

    grunt watch compiliation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,32 @@
 module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-screeps');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-run');
 
   grunt.initConfig({
-      screeps: {
-          options: {
-              email: '<yer email>',
-              password: '<yer password>',
-              branch: 'default',
-              ptr: false
-          },
-          dist: {
-              src: ['dist/*.js']
-          }
-      }
+        run: {
+            npm_build: {
+                cmd: 'npm',
+                args: ['run', 'build']
+            }
+        },
+        screeps: {
+            options: {
+                email: '<yer email>',
+                password: '<yer password>',
+                branch: 'default',
+                ptr: false
+            },
+            dist: {
+                src: ['dist/*.js']
+            }
+        },
+        watch: {
+            build: {
+                files: ['**/*.ts', '**/*.d.ts'],
+                tasks: ['run:npm_build']
+            }
+        }
   });
 }
